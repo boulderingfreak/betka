a670805f0f91074dbc44e03675d3e05379b52302
bouldering_freak <rsitek97@gmail.com>
2026-08-02 21:07:39 +0200

    style: added colors palette from https://www.iamsajid.com/ui-colors/

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,20 +1,37 @@
-export const theme = {
-  colorBlack: "#000000",
-  colorWhite: "#fff",
-  colorGray: "gray",
-  colorLightGray: "lightgray",
-  colorDarkGray: "#212121",
-  colorRed: "red",
-  colorLightBlue: "lightblue",
-  colorReact: "#58C4DC",
-  colorDarkBlue: "#001E32",
-  colorAccentOrange: "#b3752f",
-  colorDarkYellow: "#e9dc2e",
-  colorCake: "#fff3db",
-  colorDarkCake: "#f2e0ba",
-  colorDarkRed: "#4b0201",
-  colorLime: "#e8f574",
-  colorLightPurple: "#a99cf5",
-  colorLightPink: "#edb6d0",
-  colorLightBlack: "#1b1b1b",
+const darkMode = {
+  bgDark: "hsl(234 100% 4%)",
+  bg: "hsl(224 100% 7%)",
+  bgLight: "hsl(217 86% 10%)",
+  text: "hsl(216 100% 100%)",
+  textMuted: "hsl(216 78% 74%)",
+  highlight: "hsl(216 52% 43%)",
+  border: "hsl(215 70% 31%)",
+  borderMuted: "hsl(215 100% 19%)",
+  primary: "hsl(216 97% 75%)",
+  secondary: "hsl(38 66% 59%)",
+  danger: "hsl(8 63% 66%)",
+  warning: "hsl(52 49% 46%)",
+  success: "hsl(151 41% 51%)",
+  info: "hsl(217 73% 68%)",
 };
+
+const lightMode = {
+  bgDark: "hsl(220 59% 91%)",
+  bg: "hsl(220 100% 97%)",
+  bgLight: "hsl(220 100% 100%)",
+  text: "hsl(226 85% 7%)",
+  textMuted: "hsl(220 26% 31%)",
+  highlight: "hsl(220 100% 100%)",
+  border: "hsl(220 19% 53%)",
+  borderMuted: "hsl(220 27% 65%)",
+  primary: "hsl(221 49% 33%)",
+  secondary: "hsl(44 100% 14%)",
+  danger: "hsl(9 21% 41%)",
+  warning: "hsl(52 23% 34%)",
+  success: "hsl(147 19% 36%)",
+  info: "hsl(217 22% 41%)",
+};
+
+const modes = [darkMode, lightMode];
+
+export default modes;
